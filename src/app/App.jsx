8e4973efe252987d201267d0d6add76e223087ce
devmaffi <devmaffi550@gmail.com>
2022-06-25 20:20:10 +0300

    Add React Router routing
    Was created Cuisine component for cuisines page representation.
    
    NavIconButton component got improvements.
    
    Add react-router-dom@6.3.0 to prod dependencies.

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,12 +1,17 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+
 import { Categories } from '../components'
-import { Home } from '../pages'
+import { Home, Cuisine } from '../pages'
 
 function App() {
   return (
-    <>
+    <Router>
       <Categories />
-      <Home />
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cuisine/:type" element={<Cuisine />} />
+      </Routes>
+    </Router>
   )
 }
 
--- src/components/common/NavIconButton.jsx
+++ src/components/common/NavIconButton.jsx
@@ -1,11 +1,13 @@
+import { NavLink } from 'react-router-dom'
+
 import '../../styles/modules/icon-button.sass'
 
 function NavIconButton({ to, iconComponent: IconComponent, label }) {
   return (
-    <div className="icon-button" to={to}>
+    <NavLink className="icon-button" to={to}>
       <IconComponent />
       <h4>{label}</h4>
-    </div>
+    </NavLink>
   )
 }
 
