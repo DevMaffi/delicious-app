a08bc8b12faa07276b300575e21aedbdfeed2ab1
devmaffi <devmaffi550@gmail.com>
2022-06-26 12:23:20 +0300

    Implement Searched page navigation route

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,7 +1,7 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import { Filters } from '../components'
-import { Home, Cuisine } from '../pages'
+import { Home, Searched, Cuisine } from '../pages'
 
 function App() {
   return (
@@ -9,6 +9,7 @@ function App() {
       <Filters />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/searched/:query" element={<Searched />} />
         <Route path="/cuisine/:type" element={<Cuisine />} />
       </Routes>
     </Router>
--- src/components/Filters.jsx
+++ src/components/Filters.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
+import { useNavigate } from 'react-router-dom'
 
 import Categories from './Categories'
 import { Search } from './common'
 
 function Filters() {
   const [query, setQuery] = useState('')
+  const navigate = useNavigate()
 
   const inputHandler = e => {
     setQuery(e.currentTarget.value)
@@ -12,6 +14,7 @@ function Filters() {
 
   const submitHandler = e => {
     e.preventDefault()
+    navigate(`/searched/${query}`)
   }
 
   return (
